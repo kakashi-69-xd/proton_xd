607cb2471ed19160ea0793d4d3fbe6cf4c72b91a
kakashi-69-xd <trishiragolder69@outlook.com>
2023-05-28 21:51:18 +0530

    incremented functions in deno

--- xd.ts
+++ xd.ts
@@ -1,5 +1,18 @@
-import { instantiate,main } from "./lib/rs_lib.generated.js";
+import { instantiate,dev,build,start } from "./lib/rs_lib.generated.js";
 
 await instantiate();
 
 
+switch(Deno.args[0]) {
+  case "dev":case "d":
+    dev();
+  break;
+  case "build":case "b":
+    build();
+  break;
+  case "start":case "s":
+    start();
+  break;
+  default:
+  break;
+}
